f1d450ae7ecd5c697837a5ff91193887cd043d1e
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-09-19 09:36:45 -0400

    _listaProtocolos.php
    Criado o componente que carrega a tabela com e sem filtros

--- templates/_partials/_listaProtocolos.php
+++ templates/_partials/_listaProtocolos.php
@@ -1,11 +1,20 @@
-<!-- Sua Dúvida: É como se fosse um componente, fazendo analogia com React ou Vue?
-
-Sim, sua analogia com componentes do React/Vue está perfeita! É exatamente esse o conceito.
-
-Qual sua função?
-É um "componente" ou "bloco de HTML reutilizável". A única função dele é conter o código da <table> que desenha a lista de protocolos.
-
-Como se comunica?
-Ele não se comunica ativamente. Outros templates (como o busca.php e futuramente um relatorios.php) que precisarem exibir a mesma tabela irão simplesmente incluí-lo com require. O template "pai" será o responsável por preparar a variável $listaDeProtocolos que este "componente" irá usar.
-
-Próximo Passo: Quando formos construir a templates/busca.php, vamos criar a tabela de listagem. Depois, podemos mover o código da tabela para dentro deste arquivo para reutilizá-lo. -->
+<table border="1">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Número</th>
+            <th>Páginas</th>
+            <th>Data de Registro</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($listaDeProtocolos as $protocolo): ?>
+            <tr>
+                <td><?= htmlspecialchars($protocolo->id()) ?></td>
+                <td><?= htmlspecialchars($protocolo->numero()) ?></td>
+                <td><?= htmlspecialchars($protocolo->paginas()) ?></td>
+                <td><?= $protocolo->data()->format('d/m/Y H:i:s') ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
