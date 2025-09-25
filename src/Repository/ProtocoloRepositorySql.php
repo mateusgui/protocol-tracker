e6078e79d22eab067f74340b8f8cefa8c6ad7ba6
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-09-25 11:12:06 -0400

    Update ProtocoloRepositorySql.php
    public function all() - Criada função que retorna todos os protocolos ordenados por data

--- src/Repository/ProtocoloRepositorySql.php
+++ src/Repository/ProtocoloRepositorySql.php
@@ -4,15 +4,35 @@ namespace Mateus\ProtocolTracker\Repository;
 
 use Mateus\ProtocolTracker\Model\Protocolo;
 use DateTimeImmutable;
+use PDO;
 
 final class ProtocoloRepositorySql implements ProtocoloRepositoryInterface
 {
+    private PDO $connection;
+
+    public function __construct(PDO $connection)
+    {
+        $this->connection = $connection;
+    }
+
     public function all(): array
     {
         //Consulta SQL
-        //Converte cada registro retornado na consulta para um objeto do tipo Protocolo
-        //Salva todos os protocolos em um array de Protocolo
-        return []; //Retorno PADRÃO
+        $sqlQuery = "SELECT * FROM protocolos ORDER BY criado_em DESC;";
+        $stmt = $this->connection->query($sqlQuery);
+        $listaAssociativa = $stmt->fetchAll();
+
+        if (!is_array($listaAssociativa)) {
+            return [];
+        }
+
+        $listaDeProtocolos = []; 
+        foreach ($listaAssociativa as $dadosDeUmProtocolo) {
+            $protocoloObjeto = Protocolo::fromArray($dadosDeUmProtocolo); //recriando objetos Protocolo 
+            $listaDeProtocolos[] = $protocoloObjeto;
+        }
+
+        return $listaDeProtocolos;
     }
 
     public function search(?string $numero, ?DateTimeImmutable $dataInicio, ?DateTimeImmutable $dataFim): array
