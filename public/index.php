a0c2cae48e5a0e0da73cb62f3c59e507fe64577f
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-09-25 10:54:04 -0400

    Inicio migração
    Inicio da migração para sqlite

--- public/index.php
+++ public/index.php
@@ -6,67 +6,80 @@ session_start();
 require __DIR__ . '/../vendor/autoload.php';
 
 use Mateus\ProtocolTracker\Controller\WebController;
+use Mateus\ProtocolTracker\Infrastructure\Persistence\ConnectionCreator;
 use Mateus\ProtocolTracker\Model\Protocolo;
 use Mateus\ProtocolTracker\Repository\ProtocoloRepository;
 use Mateus\ProtocolTracker\Service\DashboardService;
 use Mateus\ProtocolTracker\Service\ProtocoloService;
 
-$caminhoJson = __DIR__ . '/../data/protocolos.json'; //Caminho do meu JSON de dados
-$repositorio = new ProtocoloRepository($caminhoJson); //Instanciação de ProtocoloRepository
-$dashboardService = new DashboardService($repositorio); //Instanciação de DashboardSerivce usando o $repositorio que é uma instância de ProtocoloRepository
-$protocoloService = new ProtocoloService($repositorio); //Instanciação de ProtocoloService usando o $repositorio que é uma instância de ProtocoloRepository
-$webController = new WebController($repositorio, $dashboardService, $protocoloService); //Instanciação de webController usando instâncias de: repositorio, dashboardService e protocoloService
+try {
+    // CONEXÃO COM O BANCO
+    $connection = ConnectionCreator::createConnection();
 
-// Variável para guardar possíveis erros de validação do formulário
-$erro = null;
+    $caminhoJson = __DIR__ . '/../data/protocolos.json'; //Caminho do meu JSON de dados
+    $repositorio = new ProtocoloRepository($caminhoJson); //Instanciação de ProtocoloRepository
+    $dashboardService = new DashboardService($repositorio); //Instanciação de DashboardSerivce usando o $repositorio que é uma instância de ProtocoloRepository
+    $protocoloService = new ProtocoloService($repositorio); //Instanciação de ProtocoloService usando o $repositorio que é uma instância de ProtocoloRepository
+    $webController = new WebController($repositorio, $dashboardService, $protocoloService); //Instanciação de webController usando instâncias de: repositorio, dashboardService e protocoloService
 
-// LÓGICA DE ROTEAMENTO 
-// ---------------------
-$uri = $_SERVER['REQUEST_URI'];
-$uri = parse_url($uri, PHP_URL_PATH);
-$method = $_SERVER['REQUEST_METHOD'];
+    // Variável para guardar possíveis erros de validação do formulário
+    $erro = null;
 
-switch ($uri) {
-    // ----- ROTA BASE '/' -----
-    case '/':
-        // REQUEST_METHOD = POST - Executa o bloco if
-        if ($method === 'POST') {
-            $webController->salvarNovoProtocolo(); //Registro de novo protocolo
-        } else{
-            $webController->home(); //Carregamento padrão da home com o dashboard
-        }
-        break;
-    
-    // ----- ROTA DE BUSCA '/busca' -----
-    case '/busca':
-        $webController->buscaProtocolo();
-        break;
+    // LÓGICA DE ROTEAMENTO 
+    // ---------------------
+    $uri = $_SERVER['REQUEST_URI'];
+    $uri = parse_url($uri, PHP_URL_PATH);
+    $method = $_SERVER['REQUEST_METHOD'];
 
-    // ----- ROTA DO DASHBOARD '/busca' -----
-    case '/dashboard':
-        $webController->exibirDashboard();
-        break;
+    switch ($uri) {
+        // ----- ROTA BASE '/' -----
+        case '/':
+            // REQUEST_METHOD = POST - Executa o bloco if
+            if ($method === 'POST') {
+                $webController->salvarNovoProtocolo(); //Registro de novo protocolo
+            } else{
+                $webController->home(); //Carregamento padrão da home com o dashboard
+            }
+            break;
+        
+        // ----- ROTA DE BUSCA '/busca' -----
+        case '/busca':
+            $webController->buscaProtocolo();
+            break;
 
-    // ----- ROTA DE EDIÇÃO '/editar' -----
-    case '/editar':
-        if($method === 'POST'){
-            $webController->editarProtocolo();
-        } else {
-            $webController->exibirFormularioEdicao();
-        }
-        break;
+        // ----- ROTA DO DASHBOARD '/busca' -----
+        case '/dashboard':
+            $webController->exibirDashboard();
+            break;
 
-    // ----- ROTA DE EXCLUSÃO '/excluir' (Ainda a ser criada) -----
-    case '/excluir':
-        if($method === 'POST'){
-            $webController->deletarProtocolo();
-        } else {
-            header('Location: /busca');
-        }
-        break;
+        // ----- ROTA DE EDIÇÃO '/editar' -----
+        case '/editar':
+            if($method === 'POST'){
+                $webController->editarProtocolo();
+            } else {
+                $webController->exibirFormularioEdicao();
+            }
+            break;
 
-    // Rota p
-    default:
-        $webController->notFound();
-        break;
+        // ----- ROTA DE EXCLUSÃO '/excluir' (Ainda a ser criada) -----
+        case '/excluir':
+            if($method === 'POST'){
+                $webController->deletarProtocolo();
+            } else {
+                header('Location: /busca');
+            }
+            break;
+
+        // Rota p
+        default:
+            $webController->notFound();
+            break;
+    }
+
+} catch (Exception $e) {
+    http_response_code(500);
+    echo "<h1>Erro na conexão com o banco de dados.</h1>";
+    echo "<p>Por favor, tente novamente mais tarde.</p>";
 }
+
+
--- src/Infrastructure/Persistence/ConnectionCreator.php
+++ src/Infrastructure/Persistence/ConnectionCreator.php
@@ -2,6 +2,7 @@
 
 namespace Mateus\ProtocolTracker\Infrastructure\Persistence;
 
+use Exception;
 use PDO;
 
 class ConnectionCreator
@@ -9,11 +10,12 @@ class ConnectionCreator
     public static function createConnection() : PDO
     {
         $dbPath = __DIR__ . '/../../../data/db.sqlite'; 
+
         $connection = new PDO('sqlite:' . $dbPath);
 
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
+        
         return $connection;
     }
 }
