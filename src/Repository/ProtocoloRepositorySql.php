876bf8d18d6f198499bb7b71068f9cb01e87e69a
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-09-25 10:05:45 -0400

    Create ProtocoloRepositorySql.php

---
+++ src/Repository/ProtocoloRepositorySql.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Mateus\ProtocolTracker\Repository;
+
+use Mateus\ProtocolTracker\Model\Protocolo;
+use DateTimeImmutable;
+
+final class ProtocoloRepositorySql implements ProtocoloRepositoryInterface
+{
+    public function all(): array
+    {
+        return []; //Retorno PADRÃO
+    }
+
+    public function search(?string $numero, ?DateTimeImmutable $dataInicio, ?DateTimeImmutable $dataFim): array
+    {
+        return []; //Retorno PADRÃO
+    }
+
+    public function buscaPorPeriodo(?DateTimeImmutable $dataInicio = null, ?DateTimeImmutable $dataFim = null): array
+    {
+        return []; //Retorno PADRÃO
+    }
+
+    public function buscaPorNumero(string $numero): ?Protocolo
+    {
+        return $listaDeProtocolos; //Retorno PADRÃO
+    }
+
+    public function buscaPorId(string $id): ?Protocolo
+    {
+        return $listaDeProtocolos; //Retorno PADRÃO
+    }
+
+    public function add(Protocolo $novoProtocolo): void
+    {
+        
+    }
+
+    public function update(Protocolo $protocoloParaAtualizar): bool
+    {
+        return true; //Retorno PADRÃO
+    }
+
+    public function delete(string $id): bool
+    {
+        return true; //Retorno PADRÃO
+    }
+
+}
