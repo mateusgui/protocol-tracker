9e7376e230a67f13e9e5b429e0b7a4f3c6be04a8
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-09-25 21:17:18 -0400

    Create AuditRepository.php

---
+++ src/Repository/AuditRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Mateus\ProtocolTracker\Repository;
+
+use DateTimeImmutable;
+use DateTimeZone;
+use PDO;
+use PDOStatement;
+
+final class AuditRepository
+{
+    private PDO $connection;
+
+    public function __construct(PDO $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function registraAlteracao(string $protocolo_id, int $usuario_id, string $numero_protocolo, string $acao, DateTimeImmutable $data_acao): void
+    {
+        $sqlQuery = "INSERT INTO protocolos_auditoria (protocolo_id, usuario_id, numero_protocolo, acao, data_acao) VALUES (:protocolo_id, :usuario_id, :numero_protocolo, :acao, :data_acao);";
+
+        $stmt = $this->connection->prepare($sqlQuery);
+        $stmt->bindValue(':protocolo_id', $protocolo_id);
+        $stmt->bindValue(':usuario_id', $usuario_id);
+        $stmt->bindValue(':numero_protocolo', $numero_protocolo);
+        $stmt->bindValue(':acao', $acao);
+        $stmt->bindValue(':data_acao', $data_acao->format('Y-m-d H:i:s'));
+        $stmt->execute();
+    }
+}
